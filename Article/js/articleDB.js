e694ba7f65334c6fa9e9442319df9bb602db2853
MadoshriAv <34806919+MadoshriAv@users.noreply.github.com>
2018-01-02 14:15:49 +0200

    site 4 david
    download & continue

---
+++ Article/js/articleDB.js
@@ -0,0 +1,58 @@
+					// Article Data Base //
+
+var Article1 = { 
+	Id: "001", 
+	Title: "South Korean president welcomes North Korean Olympic participation", 
+	subTitle: "South Korean President Moon Jae-in has welcomed Kim Jong Un's apparent willingness to enter into dialogue and called for swift measures to help North Korea participate in the 2018 Pyeongchang Winter Olympics during a Cabinet meeting in Seoul on Tuesday." , 
+	dateOf : "" , 
+	ratingOf :"" ,
+	articleBody: "The North Korean leader struck an unusually conciliatory note in his annual New Year's Day address Monday, declaring his hope for (a) peaceful resolution with our southern border.														In the televised address, Kim called for peace on the Korean peninsula and said North Korean representatives should start talks with their South Korean counterparts as soon as possible to discuss sending a delegation to the 2018 Winter Games, to be hosted in South Korea next month.																										Moon, who has long advocated for closer relations with the North, described Kim's remarks as a response to our proposal to turn the Pyeongchang Olympic Games into an epoch-making opportunity to improve inter-Korean relations and establish peace.			Moon said he would ask the Unification Ministry -- the government department responsible for inter-Korean relations -- and the Ministry of Culture and Sports to quickly come up with follow-up measures for the speedy restoration of South-North Korean dialogue and realize the North Korean delegation's participation in the Pyeongchang Olympics."
+	
+}
+
+var Article2 = { 
+	Id: "002", 
+	Title: "Iranian President Rouhani calls for unity as death toll rises in unrest", 
+	subTitle: "Iranian President Hassan Rouhani tried to downplay the significance of sometimes violent protests across his country that have left 12 people dead in the biggest challenge to the authority of the Tehran regime since mass demonstrations in 2009." , 
+	dateOf : "" , 
+	ratingOf :"" ,
+	articleBody: "Iranian President Rouhani calls for unity as death toll rises in unrest By Eliza Mackintosh and Alanne Orjoux, Updated 0849 GMT (1649 HKT) January 2, 2018 Iranian officials: 'Provocateurs' to be targeted . 											Now Playing Iranian officials:... Iranian officials: 'Provocateurs' to be targeted 02:08 (CNN)Iranian President Hassan Rouhani tried to downplay the significance of sometimes violent protests across his country that have left 12 people dead in the biggest challenge to the authority of the Tehran regime since mass demonstrations in 2009.												Our great nation has witnessed a number of similar incidents in the past and has comfortably dealt with them. This is nothing, Rouhani said in a meeting with Iranian members of parliament on Monday.															Rouhani has called for calm as his government deals with the widespread spontaneous uprising.																										                                                                                                    							-- A man creating a disturbance shot at police Monday with a hunting rifle, killing one officer and wounding three others, according to state media. The shooting happened in Najafabad.																					 																																-- A man creating a disturbance shot at police Monday with a hunting rifle, killing one officer and wounding three others, according to state media. The shooting happened in Najafabad.																			 																																-- Fresh protests broke out in Tehran and other cities, with amateur video showing demonstrators chanting anti-government slogans." 
+	
+}
+
+var Article3 = { 
+	Id: "003", 
+	Title: "Mariah Carey's NYE hot tea is the first meme of 2018" ,
+	subTitle: "Mariah Carey does not need your redemption talk, s'il vous plait et merci. But admittedly, her New Year's Eve performance Sunday was infinitely better than the lip-synching disaster of 2016, and it was just weird enough to provide us with our very first meme of 2018." , 
+	dateOf : "" , 
+	ratingOf :"" ,
+	articleBody: "Warmly ensconced in the bleached hide of Falkor, the golden diva and the two full-sized Tiffany chandeliers hanging from her ears wailed us into the New Year. There was confetti and sparkly nude illusion dresses and a little gospel choir and expert vocalization. It was all very on-brand.																						 																																							But perhaps nothing was more on-brand than when Mariah paused between songs to ask for some hot tea.									 																																												They told me there would be tea she said, looking perplexed. Despite the hellish cold of the evening, there was not.					 																																											Oh, it's a disaster. Okay, well, we'll just have to rough it. I'm going to be like everybody else, with no hot tea." 
+
+}
+
+var Article4 = { 
+	Id: "004", 
+	Title: "Reese Witherspoon, Shonda Rhimes, Meryl Streep among powerful women in anti-harassment group", 
+	subTitle: "A group of more than 1,000 women in entertainment have detailed a comprehensive plan to combat sexual harassment across industries." , 
+	dateOf : "" , 
+	ratingOf :"" ,
+	articleBody: "Time's Up was officially unveiled Monday in a story by The New York Times, though the group was formed not long after the first round of allegations against former movie mogul Harvey Weinstein came to light in October.						 	 																																														Director Ava DuVernay, producer Kathleen Kennedy and dozens of actors, including America Ferrera, Emma Stone and Constance Wu, laid out the mission of Time's Up in an open letter.																					 																																														The women said they were encouraged to find ways to battle harassment in all industries after receiving a letter of support from Alianza Nacional de Campesinas (The National Farmworker Women's Alliance) in November.																								 																																													In that letter, the alliance acknowledged the outpouring of stories about gender-based violence and discrimination in entertainment, but wrote, sadly, we're not surprised because it's a reality we know far too well.																						Countless farmworker women across our country suffer in silence because of the widespread sexual harassment and assault that they face at work, the alliance's letter read." 
+
+}
+
+var Article5 = { 
+	Id:"005", 
+	Title:"Puerto Vallarta: How to get this Mexican beach to yourself", 
+	subTitle:"As soon as you touch down in Puerto Vallarta, in Mexico's Jalisco state on the Pacific Coast, you'll find yourself accosted by offers of complimentary tequila shots and greeters trying to sell you timeshares. But Puerto Vallarta was once a very different destination." , 
+	dateOf : "" , 
+	ratingOf :"" ,
+	articleBody :"Known as Las Peñas in the 1800s, it was renamed after former state governor Ignacio Vallarta in the early 1900s. As a fishing town, Puerto Vallarta's main source of pesos, in addition to fishing, was agricultural exports such as bananas, coconuts and corn. These days, it relies on importing tourists, hypnotized by the green turquoise seawater. 			 	 																																														Blame Hollywood. Stars such as Richard Burton, Elizabeth Taylor and Ava Gardner descended on Banderas Bay to film John Huston's The Night of the Iguana.																																								 																																												Soon enough, the paparazzi caught whiff of Taylor and Burton's blossoming love affair with each other -- and their new second home. Others followed suit, including investors. 																																			 																																											Flash-forward several decades later and cue the mariachi bands that tame tourists with exotic festive sounds, all-inclusive resorts with sandy beaches and watered-down drinks. But the Puerto Vallarta that Burton and Taylor in fell with is so much more, if only you look beyond the confines of your resort when you travel here." 
+
+}
+
+					// array : Articles //
+
+var Articles = [Article1, Article2, Article3, Article4, Article5];
+
+
+
